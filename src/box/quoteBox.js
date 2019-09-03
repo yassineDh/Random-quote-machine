912a557cdb63bb13c3e4ad2c1d07c41ceb3a71c7
YassineDH <yassine.dahane96@gmail.com>
2019-09-03 10:41:27 +0100

    commit the box file

---
+++ src/box/quoteBox.js
@@ -0,0 +1,17 @@
+import React ,{Component} from 'react';
+import './quoteBox.css';
+
+
+class Box extends Component {
+    render(){
+      return (
+        <div className="card" id="shape">
+          <div className="card-body">
+            Hello World !
+          </div>
+        </div>
+      );
+    }
+  }
+  
+ export default Box;
