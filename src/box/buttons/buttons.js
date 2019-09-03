faca4e7810987684faaa88316eff65038902fd08
YassineDH <yassine.dahane96@gmail.com>
2019-09-03 19:43:26 +0100

    Show the quote in the log console

--- src/box/buttons/buttons.js
+++ src/box/buttons/buttons.js
@@ -9,17 +9,26 @@ class Quotebuttons extends Component {
         this.state = {
             allquotes: {...Quotedata}
         };
+        this.handleClickChange = this.handleClickChange.bind(this);
       }
+
+    handleClickChange(){
+        let len = this.state.allquotes.quotes.length;
+        let num = Math.floor((Math.random() * len) + 1);
+        console.log(this.state.allquotes.quotes[num].quote)
+    }
 
     render(){
       return (
         <div>
             <div>
-                {this.state.allquotes.quotes[20].quote}
+                {this.state.allquotes.quotes[20].quote} 
+
+
             </div>
           <button className="btn btn-primary">Twitter</button>
           <button className="btn btn-primary">Tumblr</button>
-          <button className="btn btn-primary">Change Quote</button>
+          <button className="btn btn-primary" onClick={this.handleClickChange}>Change Quote</button>
         </div>
       );
     }
