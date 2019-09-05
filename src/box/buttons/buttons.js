e081d649ac367e15278c4ece78fc6824bc3fa9df
YassineDH <yassine.dahane96@gmail.com>
2019-09-05 19:46:32 +0100

    added the twitter icon

--- src/box/buttons/buttons.js
+++ src/box/buttons/buttons.js
@@ -6,7 +6,7 @@ class Quotebuttons extends Component {
     render(){
       return (
         <div>
-          <button className="btn btn-primary">Twitter</button>
+          <button className="btn"><span><i className="fa fa-twitter"></i></span></button>
           <button className="btn btn-primary">Tumblr</button>
           <button className="btn btn-primary" onClick={this.props.newOne}>Change Quote</button>
         </div>
